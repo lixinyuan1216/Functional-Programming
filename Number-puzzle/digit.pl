34c58e6306b6892b651d648063e4878ac5975f12
lixinyuan1216 <lixinyuan1216@gmail.com>
2015-11-21 16:17:57 +1100

    Create digit.pl

---
+++ Number-puzzle/digit.pl
@@ -0,0 +1,17 @@
+%  File     : digit.pl
+%  Author   : Xinyuan Li
+%  Purpose  : Holds digit set among which numbers vary in [1,9]. 
+
+% Facts about digit.
+% ---------------------
+% digit(Number)
+
+digit(1).
+digit(2).
+digit(3).
+digit(4).
+digit(5).
+digit(6).
+digit(7).
+digit(8).
+digit(9).
